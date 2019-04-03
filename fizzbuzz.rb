0a7c3c5f9c4ee83cf2d8022a4d92eca914d94dc5
Jasterix C <loune.calixte@gmail.com>
2019-04-03 05:00:26 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -5,7 +5,7 @@ require 'pry'
 def fizzbuzz(int)
   if int%3==0 and int%5 == 0 
     "FizzBuzz"
-    binding.pry 
+   # binding.pry 
   elsif int % 3 == 0 # if the number int is divisible by 3
     "Fizz" # Go fizz
   elsif int % 5 == 0 
